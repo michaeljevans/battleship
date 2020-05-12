2e1e2882b3356902c58acf1dad8ce2c6b3999e97
Michael Evans <michaeljevans113@gmail.com>
2020-05-11 21:45:05 -0600

    Add tests for .hit and .sunk? methods

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -8,4 +8,35 @@ class ShipTest < MiniTest::Test
 
     assert_instance_of Ship, cruiser
   end
+
+  def test_hit_subtracts_health_from_ship
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal 2, cruiser.hit
+    assert_equal 1, cruiser.hit
+    assert_equal 0, cruiser.hit
+    assert_equal 0, cruiser.hit
+  end
+
+  def test_has_it_sunk
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal false, cruiser.sunk?
+
+    cruiser.hit
+
+    assert_equal false, cruiser.sunk?
+
+    cruiser.hit
+
+    assert_equal false, cruiser.sunk?
+
+    cruiser.hit
+
+    assert_equal true, cruiser.sunk?
+
+    cruiser.hit
+
+    assert_equal true, cruiser.sunk?
+  end
 end

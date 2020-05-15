6c2a14a249a758fcece92b6c44d5089792785394
Michael Evans <michaeljevans113@gmail.com>
2020-05-14 20:10:57 -0600

    Current progress

--- test/board_test.rb
+++ test/board_test.rb
@@ -17,13 +17,16 @@ class BoardTest < MiniTest::Test
     board = Board.new
     board.cells
 
-    assert_equal true, board.valid_coordinates?("A1")
-    assert_equal false, board.valid_coordinates?("A6")
-    assert_equal false, board.valid_coordinates?("D11")
-    assert_equal true, board.valid_coordinates?("a1")
+    assert_equal true, board.valid_coordinate?("A1")
+    assert_equal true, board.valid_coordinate?("D4")
+    assert_equal false, board.valid_coordinate?("A5")
+    assert_equal false, board.valid_coordinate?("E1")
+    assert_equal false, board.valid_coordinate?("A22")
+    assert_equal true, board.valid_coordinate?("c3")
   end
 
   def test_placement_invalid_if_coordinate_array_length_and_ship_length_are_different
+    skip
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
@@ -33,10 +36,10 @@ class BoardTest < MiniTest::Test
     assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
     assert_equal true, board.valid_placement?(cruiser, ["A1", "A2", "A3"])
     assert_equal true, board.valid_placement?(submarine, ["A1", "A2"])
+    assert_equal true, board.valid_placement?(cruiser, ["B1", "C1", "D1"])
   end
 
   def test_placement_invalid_if_coordinate_array_elements_are_not_consecutive
-    skip
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
@@ -44,11 +47,12 @@ class BoardTest < MiniTest::Test
 
     assert_equal false, board.valid_placement?(cruiser, ["A1", "A2", "A4"])
     assert_equal false, board.valid_placement?(submarine, ["A1", "C1"])
-    assert_equal true, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
-    assert_equal true, board.valid_placement?(submarine, ["C1", "B1"])
+    assert_equal false, board.valid_placement?(cruiser, ["A3", "A2", "A1"])
+    assert_equal false, board.valid_placement?(submarine, ["C1", "B1"])
   end
 
   def test_placement_invalid_if_coordinate_array_elements_are_diagonal
+    skip
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
     submarine = Ship.new("Submarine", 2)
@@ -57,6 +61,8 @@ class BoardTest < MiniTest::Test
     assert_equal false, board.valid_placement?(cruiser, ["A1", "B2", "C3"])
     assert_equal false, board.valid_placement?(submarine, ["C2", "D3"])
     assert_equal true, board.valid_placement?(cruiser, ["A1", "B1", "C1"])
+    assert_equal true, board.valid_placement?(submarine, ["A1", "B1"])
+    assert_equal false, board.valid_placement?(cruiser, ["A4", "B3", "C2"])
   end
 
   def test_valid_ship_placement
@@ -68,5 +74,7 @@ class BoardTest < MiniTest::Test
 
     assert_equal true, board.valid_placement?(submarine, ["A1", "A2"])
     assert_equal true, board.valid_placement?(cruiser, ["B1", "C1", "D1"])
+    assert_equal false, board.valid_placement?(cruiser, ["A5", "A6", "A7"])
+    assert_equal false, board.valid_placement?(submarine, ["A4", "A5"])
   end
 end

5243d6e59d2f892869956780868a7a93edc66cec
Michael Evans <michaeljevans113@gmail.com>
2020-05-16 13:15:25 -0600

    Revise test assertions for more precise results (assert/refute vs. assert_equal true/false)

--- test/board_test.rb
+++ test/board_test.rb
@@ -3,7 +3,6 @@ require 'minitest/pride'
 require './lib/ship'
 require './lib/cell'
 require './lib/board'
-require 'pry'
 
 class BoardTest < MiniTest::Test
   def test_it_exists
@@ -33,9 +32,6 @@ class BoardTest < MiniTest::Test
 
     assert_equal false, board.valid_placement?(cruiser, ["A1", "A2"])
     assert_equal false, board.valid_placement?(submarine, ["A2", "A3", "A4"])
-    assert_equal true, board.valid_placement?(cruiser, ["A1", "A2", "A3"])
-    assert_equal true, board.valid_placement?(submarine, ["A1", "A2"])
-    assert_equal true, board.valid_placement?(cruiser, ["B1", "C1", "D1"])
   end
 
   def test_placement_invalid_if_coordinate_array_elements_are_not_consecutive
@@ -58,9 +54,6 @@ class BoardTest < MiniTest::Test
 
     assert_equal false, board.valid_placement?(cruiser, ["A1", "B2", "C3"])
     assert_equal false, board.valid_placement?(submarine, ["C2", "D3"])
-    assert_equal true, board.valid_placement?(cruiser, ["A1", "B1", "C1"])
-    assert_equal true, board.valid_placement?(submarine, ["A1", "B1"])
-    assert_equal false, board.valid_placement?(cruiser, ["A4", "B3", "C2"])
   end
 
   def test_valid_ship_placement
@@ -86,5 +79,19 @@ class BoardTest < MiniTest::Test
     assert_equal board.cells["A1"].ship, board.cells["A2"].ship
     assert_equal board.cells["A2"].ship, board.cells["A3"].ship
     assert_equal board.cells["B2"].ship, board.cells["C2"].ship
+  end
+
+  def test_multiple_ships_cannot_be_placed_on_the_same_coordinates
+    board = Board.new
+    cruiser = Ship.new("Cruiser", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    board.place(cruiser, ["A1", "A2", "A3"])
+
+    assert_equal false, board.valid_placement?(submarine, ["A1", "B1"])
+  end
+
+  def test_it_renders
+    skip
   end
 end

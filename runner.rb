7b8c033698f9cab84fcc9db46d98bdf6feb42479
Michael Evans <michaeljevans113@gmail.com>
2020-05-19 15:33:10 -0600

    Remove minitest require statements

--- runner.rb
+++ runner.rb
@@ -1,11 +1,8 @@
-require 'minitest/autorun'
-require 'minitest/pride'
 require './lib/cell'
 require './lib/ship'
 require './lib/board'
 require './lib/turn'
 require './lib/menu'
-require 'pry'
 
 run_menu = Menu.new
 run_menu.main_menu
